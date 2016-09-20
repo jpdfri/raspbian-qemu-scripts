0e2cd50b8a329cc9c4093a37366922e75b32bbd0
Stuart Axon <stuaxo2@yahoo.com>
2016-09-20 13:50:08 +0100

    Fixup paths in run_qemu.sh

--- run_qemu.sh
+++ run_qemu.sh
@@ -6,5 +6,6 @@ NETWORK="-net nic -net user"
 RASPBIAN_BUILD=2016-05-27-raspbian-jessie-lite
 RASPBIAN_IMAGE=${RASPBIAN_BUILD}.img
 
+TMPDIR=/tmp/raspbian
 
-qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -usbdevice tablet -display sdl -append "root=/dev/sda2 panic=2 rootfstype=ext4 rw init=bin/bash" -drive format=raw,file=${RASPBIAN_IMAGE} ${NETWORK}
+qemu-system-arm -kernel ${TMPDIR}/kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -usbdevice tablet -display sdl -append "root=/dev/sda2 panic=2 rootfstype=ext4 rw init=bin/bash" -drive format=raw,file=${TMPDIR}/${RASPBIAN_IMAGE} ${NETWORK}
