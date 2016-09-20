f946de858119c0f929ccc97627c9eb3f017b6a6b
Stuart Axon <stuaxo2@yahoo.com>
2016-09-20 13:36:57 +0100

    Fix paths in mount / umount

--- umount-raspbian.sh
+++ umount-raspbian.sh
@@ -7,11 +7,4 @@ TMPDIR=/tmp/raspbian
 MOUNT_DIR=${TMPDIR}/mnt
 LOOP_DEV=$(sudo losetup -f -P --show ${TMPDIR}/${RASPBIAN_IMAGE})
 
-
-mkdir -p ${MOUNT_DIR}
-
-echo Mount ${LOOP_DEV}p2 ${MOUNT_DIR}
-sudo mount ${LOOP_DEV}p2 -o rw ${MOUNT_DIR}
-
-echo To unmount:
-echo sudo umount ${LOOP_DEV}p2 && sudo losetup -D ${LOOP_DEV}
+sudo umount ${MOUNT_DIR}
