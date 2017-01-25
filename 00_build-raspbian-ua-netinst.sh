802041c6a018c3495241772701a29c669ea1ec0b
Julien Pinget <jpdfri@users.noreply.github.com>
2017-01-25 21:45:32 +0100

    Build Raspbian netinst - add CLI argument for custom folders

--- 00_build-raspbian-ua-netinst.sh
+++ 00_build-raspbian-ua-netinst.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-RUI_FOLDER="raspbian-ua-netinst"
+# Script to automatically build raspbian-ua-netinst
+
+# Check for the cloned repo
+if [ -z $1 ]; then
+	echo "Assuming \"raspbian-ua-netinst\" as the target directory to build the Raspbian netinstaller image."
+	RUI_FOLDER="raspbian-ua-netinst"
+else
+	echo "Using "$1" as the target directory to build the Raspbian netinstaller image."
+	RUI_FOLDER=$1
+fi
+
 RUI_PATH=$(find ~/ -name "${RUI_FOLDER}" 2>/dev/null)
+
 if [ -z "${RUI_PATH}" ]; then
-	echo "No ${RUI_FOLDER} folder found. Please clone it from Github. Aborting."
+	echo "No ${RUI_FOLDER} folder found. Please clone raspbian-ua-netinst from Github or provide the relevant folder name as an argument. Aborting."
 	exit 1
 fi
+
 cd ${RUI_PATH}
+
+# Start building
 echo "Cleaning previous build files."
 sudo bash -c "./clean.sh"
 echo "Updating packages."
